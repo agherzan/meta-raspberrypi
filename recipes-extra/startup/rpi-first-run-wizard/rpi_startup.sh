06630f323d9254f23258a196de2a52fd8008a2ef
Paul Barker <paul@paulbarker.me.uk>
2013-05-22 10:06:03 +0100

    rpi-first-run-wizard: remove
    The existing first run script is inappropriate for a board support layer. It is
    also full of references to OpenPandora and is not portable across all distros
    which may use meta-raspberrypi.
    
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- recipes-extra/startup/rpi-first-run-wizard/rpi_startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-OP_CHECKFILE='/etc/rpi/first-boot'
-OP_FIRSTRUN='xinit /usr/rpi/scripts/first-run-wizard.sh'
-
-[ -f $OP_CHECKFILE ] && echo -e "\nOP_STARTUP: $OP_CHECKFILE exists, not first boot." || $OP_FIRSTRUN
